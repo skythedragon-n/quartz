406a6843461d64fe037d885f92c5ed49e335d7f2
skythedragon <skythedragon@sdf.org>
2026-02-04 19:22:45 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// copyleft 🄯 2026 by SkyTheDragon
+// licenced under the GPLv3-or-later, details located in LICENSE
+//
+// Created by skythedragon on 2/4/26.
+//
+
+
+#include <iostream>
+
+int main() {
+
+}
