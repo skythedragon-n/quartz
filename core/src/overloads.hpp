1615d7e478a47ab9b9165904cb8244094727945f
skythedragon <skythedragon@sdf.org>
2026-03-13 11:14:44 -0700

    Added match method to overloads.hpp

--- core/src/overloads.hpp
+++ core/src/overloads.hpp
@@ -1,7 +1,23 @@
 
 #pragma once
 
+#include <variant>
+
 template<typename... Tp>
-struct overloads : Tp... { using Tp::operator()...; };
+struct Overloads : Tp... { using Tp::operator()...; };
 template<typename... Tp>
-overloads(Tp...) -> overloads<Tp...>;
+Overloads(Tp...) -> Overloads<Tp...>;
+
+namespace qtil {
+    template<typename Variant, typename... Tp>
+    decltype(auto) match(Variant&& var, Tp&&... overloads) {
+        return ::std::visit(
+            Overloads{::std::forward<Tp>(overloads)...},
+            ::std::forward<Variant>(var));
+    }
+
+    template<typename Variant, typename... Tp>
+    decltype(auto) match_overloads(Variant&& var, Overloads<Tp...> overloads) {
+        return ::std::visit(::std::move(overloads), ::std::forward<Variant>(var));
+    }
+}
