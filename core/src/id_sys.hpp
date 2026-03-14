532b5af14d7839946642a9a4bbffdd04addd96c3
skythedragon <skythedragon@sdf.org>
2026-03-14 08:55:50 -0700

    Added new Id system with IdContainer.

--- core/src/id_sys.hpp
+++ core/src/id_sys.hpp
@@ -8,13 +8,17 @@
 #pragma once
 
 #include <cstddef>
+#include <expected>
+#include <vector>
+
+#include "core_errors.hpp"
 
 /**
  * @file id_sys.hpp
  *
  * @authors SkyTheDragon
  *
- * Contains various utilities for the library system, along with all Id types.
+ * Contains the Id system
  */
 namespace quartz::core {
     class AnimFile;
@@ -27,12 +31,83 @@ namespace quartz::core {
     class Drawing;
     struct Instance;
 
+    namespace detail {
+        struct IdStorage {
+            size_t id;
+            size_t gen;
+            AnimFile* file;
+        };
+    }
+
+    enum class ObjectStage {
+        Alive,
+        Ghosted,
+        DeadWRef,
+        Dead
+    };
+
     struct IdKey {
     private:
         IdKey() = default;
         template<typename>
+        friend class IdContainer;
         friend class AnimFile;
     };
+
+    template<typename Tag>
+    class TypedId {
+    public:
+        TypedId() = default;
+
+        friend bool operator==(const TypedId&, const TypedId&) = default;
+        friend bool operator!=(const TypedId&, const TypedId&) = default;
+
+        operator bool() const { return storage_.id != static_cast<size_t>(-1) && storage_.file != nullptr; }
+
+        [[nodiscard]] bool is_valid() const {
+            return storage_.id != static_cast<size_t>(-1) && storage_.file != nullptr;
+        }
+
+    private:
+        detail::IdStorage storage_;
+
+        TypedId(size_t id, AnimFile* file, size_t gen = 0) noexcept
+            : storage_(id, gen, file) {}
+
+        TypedId(detail::IdStorage storage) noexcept
+            : storage_(storage) {}
+
+        template<typename>
+        friend class IdContainer;
+    };
+
+    template<typename T>
+    struct IdTag;
+
+    struct SymbolTag {};
+    struct InstanceTag {};
+    struct FolderTag {};
+    struct LibraryTag {};
+    struct FrameTag {};
+    struct AnimLayerTag {};
+
+    template<>
+    struct IdTag<Symbol> { using type = SymbolTag; };
+
+    template<>
+    struct IdTag<Instance> { using type = InstanceTag; };
+
+    template<>
+    struct IdTag<LibraryFolder> { using type = FolderTag; };
+
+    template<>
+    struct IdTag<Library> { using type = LibraryTag; };
+
+    template<>
+    struct IdTag<FolderTag> { using type = FolderTag; };
+
+    template<>
+    struct IdTag<AnimLayerTag> { using type = AnimLayerTag; };
 
     /**
      * @page ids ID explanation
@@ -42,6 +117,73 @@ namespace quartz::core {
      * DO NOT SET THESE MEMBERS UNDER ANY CIRCUMSTANCES. SUCH IS UNDEFINED BEHAVIOR, AND COULD CAUSE ANYTHING TO HAPPEN,
      * INCLUDING, BUT NOT LIMITED TO YOUR COMPUTER FILLING YOUR HOME WITH AN INORDINATE NUMBER OF PUPPIES & RAINBOWS.
      */
+
+    template<typename T>
+    using Id = TypedId<typename IdTag<T>::type>;
+
+    template<typename T>
+    class IdContainer {
+        struct Container {
+            T object;
+            size_t generation = 1;
+            bool free = false;
+        };
+
+        ::std::vector<Container> data_;
+        ::std::vector<size_t> freelist_;
+        AnimFile* file_ = nullptr;
+    public:
+
+        IdContainer(AnimFile* file) : file_(file) {}
+
+        static Id<T> invalid() { return Id<T>{}; }
+
+        void free(Id<T> id) {
+            if (is_valid(id)) {
+                data_[id.storage_.id].free = true;
+                freelist_.push_back(id.storage_.id);
+            }
+        }
+
+        template<typename... Args>
+        Id<T> add(Args... args) {
+            if (freelist_.empty()) {
+                data_.emplace_back(args..., Id<T>{data_.size(), file_});
+                return Id<T>{data_.size() - 1, file_};
+            }
+
+            size_t id = freelist_.back();
+            size_t gen = data_[id].generation + 1;
+            freelist_.pop_back();
+            data_[id] = Container{T{IdKey{}, file_, args..., Id<T>{id, file_, gen}}, gen};
+
+            return Id<T>{id, file_, gen};
+        }
+
+        ::std::expected<T*, ResolveFailure> resolve(Id<T> id) {
+            if (!id) {
+                return ::std::unexpected(ResolveFailure::InvalidId);
+            }
+
+            if (id.storage_.file != file_) {
+                return ::std::unexpected(ResolveFailure::WrongFile);
+            }
+
+            if (id.storage_.id >= data_.size()) {
+                return ::std::unexpected(ResolveFailure::NoSuchObject);
+            }
+
+            if (data_[id.storage_.id].free) {
+                return ::std::unexpected(ResolveFailure::TargetDeleted);
+            }
+
+            if (data_[id.storage_.id].generation != id.storage_.gen) {
+                return ::std::unexpected(ResolveFailure::TargetDeleted);
+            }
+
+            return &data_[id.storage_.id].object;
+        }
+    };
 
     struct SymbolId {size_t id; AnimFile* file = nullptr; };
     struct InstanceId { size_t id; AnimFile* file = nullptr; };
