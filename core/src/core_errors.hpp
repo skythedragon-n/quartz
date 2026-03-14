1b2eab560d0295a3c9c177b0f679da6185429136
skythedragon <skythedragon@sdf.org>
2026-03-14 08:58:22 -0700

    Ok, forgot to Commit adding this variant, and I'm tired rearranging commits, so just deal with it.

--- core/src/core_errors.hpp
+++ core/src/core_errors.hpp
@@ -17,6 +17,12 @@ namespace quartz::core {
 
     /**
      * @brief represents an error finding a symbol or folder
+     *
+     * An enum with variants to represent the various ways you can fail to find a resource in the file
+     *
+     * Variants:
+     *     - NoSuchPath: The path is not present in the tree
+     *     - InvalidPath: The path is poorly formatted
      */
     enum class FindFailure {
         NoSuchPath,
@@ -24,19 +30,13 @@ namespace quartz::core {
     };
 
     /**
-    * @deprecated Gonna be replaced with `::std::expected` soon
-    */
-    template<typename T>
-    using ActionResult = ::std::variant<success_t, T>;
-
-    /**
      * @brief Represents a failure to move a symbol or folder
      *
      * An enum with variants for each way a symbol can fail to be moved.
      *
      * Variants:
-     *      - NoSuchSource means the thing to be moved is non-existent
-     *      - NoSuchDestination meaning the destination folder is non-existent
+     *      - NoSuchSource: The thing to be moved is non-existent
+     *      - NoSuchDestination: The destination folder is non-existent
      */
     enum class MoveFailure {
         NoSuchSource,
@@ -49,22 +49,41 @@ namespace quartz::core {
      * An enum with variants for each way an object resolution can fail
      *
      * Variants:
-     *      - InvalidId means the Id's file is `nullptr`
-     *      - WrongFile means the Id is being resolved with the wrong file
-     *      - NoSuchObject means that there isn't an item in the file index for that object.
+     *      - InvalidId: The Id's file is `nullptr`
+     *      - WrongFile: the Id is being resolved with the wrong file
+     *      - NoSuchObject: that there isn't an item in the file index for that object.
+     *      - TagetDeleted: the Id once pointed its target, but target has since been deleted
      */
     enum class ResolveFailure {
         InvalidId,
         WrongFile,
-        NoSuchObject
+        NoSuchObject,
+        TargetDeleted
     };
 
+    /**
+     * @brief Represents a failure to add an object to a LibraryFolder or Library
+     *
+     * An enum with variants to represent various possible failures to add something to a LibraryFolder or Library
+     *
+     * Variants:
+     *     - NameInUse: the name has well, been used
+     */
     enum class AddFailure {
         NameInUse
     };
 
+    /**
+     * @brief Represents a failure to perform a shallow rename
+     *
+     * An enum with variants to represent various possible ways a shallow rename can fail
+     *
+     * Variants:
+     *     - SourceNonexistent: The thing that you are trying to rename actually doesn't exist in the target folder
+     *     - NameInUse: The new name to be used is already in use
+     */
     enum class RenameFailure {
-        SourceNonexistant,
+        SourceNonexistent,
         NameInUse
     };
 }
