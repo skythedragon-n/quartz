fa593ae6b1c8ca69c590d95f4d4abd0dda552627
skythedragon <skythedragon@sdf.org>
2026-03-15 14:36:47 -0700

    Added vector utility

---
+++ qtil/src/vector_util.hpp
@@ -0,0 +1,21 @@
+//
+// copyleft 🄯 2026 by SkyTheDragon
+// licenced under the GPLv3-or-later, details located in LICENSE
+//
+// Created by skythedragon on 15 Mar 2026.
+//
+
+#pragma once
+
+#include <vector>
+
+namespace qtil {
+    template<typename T>
+    ::std::vector<T>::iterator iter_at(::std::vector<T> vec, size_t index) {
+        auto it = vec.begin();
+
+        ::std::advance(it, index);
+
+        return it;
+    }
+}
