22311f12a0c29bdb185ef761a98c87137d0a9a0b
skythedragon <skythedragon@sdf.org>
2026-03-15 17:24:54 -0700

    Added a basic API for manipulating layers

---
+++ lib/src/monolayer_timeline_system.cpp
@@ -0,0 +1,234 @@
+//
+// copyleft 🄯 2026 by SkyTheDragon
+// licenced under the GPLv3-or-later, details located in LICENSE
+//
+// Created by skythedragon on 08 Mar 2026.
+//
+
+#include "./monolayer_timeline_system.hpp"
+
+#include <numeric>
+#include <ranges>
+
+#include <qtil/panic.hpp>
+#include <qtil/vector_util.hpp>
+#include <quartz/core/Symbol.hpp>
+#include <quartz/core/AnimFile.hpp>
+#include <quartz/core/AnimatedLayer.hpp>
+
+namespace quartz::lib {
+    ::std::expected<core::AnimLayerId, LayerOpError>
+    add_layer(core::AnimFile& file, core::SymbolId target, std::string name, core::Color color) {
+        auto sym = file.symbols.resolve(target);
+
+        if (!sym) {
+            return ::std::unexpected(sym.error());
+        }
+
+        core::Symbol* target_sym = *sym;
+
+        if (!target_sym->is<core::symbol_types::LayeredAnimation>()) {
+            return ::std::unexpected(LayerOpFailure::SymbolWrongType);
+        }
+
+        auto layers = *target_sym->get<core::symbol_types::LayeredAnimation>();
+
+        core::AnimLayerId layer = file.layers.add(name, color, target);
+
+        layers->layers.emplace_back(layer);
+
+        return layer;
+    }
+
+    ::std::expected<void, core::ResolveFailure>
+    delete_layer(core::AnimFile& file, core::AnimLayerId target) {
+        auto layer_res = file.layers.resolve(target);
+
+        if (!layer_res) {
+            return ::std::unexpected(layer_res.error());
+        }
+
+        auto layer = layer_res.value();
+
+        auto symbol_res = file.symbols.resolve(layer->parent());
+
+        if (!symbol_res) {
+            return ::std::unexpected(symbol_res.error());
+        }
+
+        core::Symbol* symbol = *symbol_res;
+
+        if (!symbol->is<core::symbol_types::LayeredAnimation>()) {
+            ::qtil::panic("Symbol's type does not match layer which claims it as parent!");
+        }
+
+        auto layers = *symbol->get<core::symbol_types::LayeredAnimation>();
+
+        for (auto [i, id] : ::std::views::enumerate(layers->layers)) {
+            if (id == target) {
+                layers->layers.erase(::qtil::iter_at(layers->layers, i));
+                file.layers.free(target);
+                return {};
+            }
+        }
+
+        ::qtil::panic("Layer who claims Symbol as parent not found in Symbol!");
+    }
+
+    ::std::expected<void, core::ResolveFailure>
+    set_layer_name(core::AnimFile& file, core::AnimLayerId target, std::string name) {
+        auto layer_res = file.layers.resolve(target);
+
+        if (!layer_res) {
+            return ::std::unexpected(layer_res.error());
+        }
+
+        auto layer = layer_res.value();
+
+        layer->set_name(name);
+
+        return {};
+    }
+
+    ::std::expected<void, core::ResolveFailure>
+    set_layer_color(core::AnimFile& file, core::AnimLayerId target, core::Color color) {
+        auto layer_res = file.layers.resolve(target);
+
+        if (!layer_res) {
+            return ::std::unexpected(layer_res.error());
+        }
+
+        core::AnimatedLayer* layer = layer_res.value();
+
+        layer->set_color(color);
+
+        return {};
+    }
+
+    ::std::expected<void, LayerOpError>
+    move_layer(core::AnimFile& file, core::SymbolId symbol, size_t source, size_t target) {
+        auto symbol_res = file.symbols.resolve(symbol);
+
+        if (!symbol_res) {
+            return ::std::unexpected(symbol_res.error());
+        }
+
+        core::Symbol* symbol_p = *symbol_res;
+
+        if (!symbol_p->is<core::symbol_types::LayeredAnimation>()) {
+            return ::std::unexpected(LayerOpFailure::SymbolWrongType);
+        }
+
+        if (source == target) {
+            return {};
+        }
+
+        auto layers = *symbol_p->get<core::symbol_types::LayeredAnimation>();
+
+        if (source >= layers->layers.size()) {
+            return ::std::unexpected(LayerOpFailure::OutOfBoundsLayerIndex);
+        }
+
+        core::AnimLayerId layer = layers->layers[source];
+
+        layers->layers.erase(layers->layers.begin() + source);
+
+        if (target > source) {
+            target--;
+        }
+
+        if (target >= layers->layers.size()) {
+            return ::std::unexpected(LayerOpFailure::OutOfBoundsLayerIndex);
+        }
+
+        layers->layers.emplace(layers->layers.begin() + target, layer);
+
+        return {};
+    }
+
+    ::std::expected<core::FrameId, core::ResolveFailure>
+    duplicate_frame(core::AnimFile& file, core::FrameId frame) {
+        auto frame_res = file.frames.resolve(frame);
+
+        if (!frame_res) {
+            return ::std::unexpected(frame_res.error());
+        }
+
+        core::Frame* frame_p = *frame_res;
+
+        return file.frames.add(frame_p->data());
+    }
+
+    ::std::expected<core::FrameId, FrameOpError>
+    add_keyframe(core::AnimFile& file, const core::AnimLayerId& target, const core::Frame::FrameData& data, size_t index) {
+        auto layer_res = file.layers.resolve(target);
+
+        if (!layer_res) {
+            return ::std::unexpected(layer_res.error());
+        }
+
+        core::AnimatedLayer* layer = layer_res.value();
+
+        if (layer->frames.size() == 0) {
+            return ::std::unexpected(FrameOpFailure::OutOfBoundsOp);
+        }
+
+        if (layer->frames[index].from_first == 0) {
+            return ::std::unexpected(FrameOpFailure::AlreadyKframe);
+        }
+
+        core::FrameId frame = file.frames.add(data);
+
+        auto insert_res = layer->frames.insert_frame(index, frame);
+
+        if (!insert_res) {
+            if (index >= layer->frames.size()) {
+                ::qtil::panic("FrameContainer misbehaving, insert at end returned error");
+            }
+
+            ::qtil::panic("FrameContainer misbehaving, normal insert returned error");
+        }
+
+        return frame;
+    }
+
+    ::std::expected<core::FatFrame, FrameOpError>
+    get_frame(core::AnimFile& file, core::AnimLayerId target, size_t frame) {
+        auto layer_res = file.layers.resolve(target);
+
+        if (!layer_res) {
+            return ::std::unexpected(layer_res.error());
+        }
+
+        core::AnimatedLayer* layer = layer_res.value();
+
+        if (frame >= layer->frames.size()) {
+            return ::std::unexpected(FrameOpFailure::OutOfBoundsOp);
+        }
+
+        return layer->frames[frame];
+    }
+
+    ::std::expected<::std::vector<core::FatFrame>, FrameOpError>
+    get_frames(core::AnimFile& file, core::AnimLayerId target, size_t frame, size_t count) {
+        auto layer_res = file.layers.resolve(target);
+
+        if (!layer_res) {
+            return ::std::unexpected(layer_res.error());
+        }
+
+        core::AnimatedLayer* layer = layer_res.value();
+
+        if (layer->frames.size() >= frame || layer->frames.size() > frame + count) {
+            return ::std::unexpected(FrameOpFailure::OutOfBoundsOp);
+        }
+
+        ::std::vector<core::FatFrame> return_value;
+
+        for (size_t i : ::std::views::iota(frame, frame + count)) {
+            return_value.emplace_back(layer->frames[i]);
+        }
+
+        return return_value;
+    }
+}
---
+++ lib/src/monolayer_timeline_system.hpp
@@ -0,0 +1,77 @@
+//
+// copyleft 🄯 2026 by SkyTheDragon
+// licenced under the GPLv3-or-later, details located in LICENSE
+//
+// Created by skythedragon on 08 Mar 2026.
+//
+
+#pragma once
+
+#include <expected>
+#include <string>
+#include <variant>
+#include <quartz/core/AnimFile.hpp>
+#include <quartz/core/id_sys.hpp>
+#include <quartz/core/drawing_utils.hpp>
+
+namespace quartz::lib {
+    enum class LayerOpFailure {
+        SymbolWrongType,
+        OutOfBoundsLayerIndex,
+    };
+
+    enum class FrameOpFailure {
+        AlreadyKframe,
+        OutOfBoundsOp,
+    };
+
+    // Move these one day, but don't delete these comments without SkyTheDragon's approval
+    // rgb(255, 0, 156)
+    // rgb(255, 0, 0)
+    // rgb(242, 139, 0)
+    // rgb(17, 202, 0)
+    // rgb(0, 216, 169)
+    // rgb(0, 183, 255)
+    // rgb(94, 122, 255)
+    // rgb(233, 51, 255)
+
+    struct FrameSelection {
+        core::AnimFile& file;
+        core::FrameContainer<core::FrameId> container;
+    };
+
+    using LayerOpError = std::variant<core::ResolveFailure, LayerOpFailure>;
+
+    using FrameOpError = ::std::variant<FrameOpFailure, core::ResolveFailure>;
+
+    ::std::expected<core::AnimLayerId, LayerOpError>
+    add_layer(core::AnimFile& file, core::SymbolId target, std::string name, core::Color color);
+
+    ::std::expected<void, core::ResolveFailure>
+    delete_layer(core::AnimFile& file, core::AnimLayerId target);
+
+    ::std::expected<void, core::ResolveFailure>
+    set_layer_name(core::AnimFile& file, core::AnimLayerId target, std::string name);
+
+    ::std::expected<void, core::ResolveFailure>
+    set_layer_color(core::AnimFile& file, core::AnimLayerId target, core::Color color);
+
+    ::std::expected<void, LayerOpError>
+    move_layer(core::AnimFile& file, core::SymbolId symbol, size_t source, size_t target);
+
+    ::std::expected<core::FrameId, core::ResolveFailure>
+    duplicate_frame(core::AnimFile& file, core::FrameId frame);
+
+    ::std::expected<core::FrameId, FrameOpError>
+    add_keyframe(core::AnimFile& file, const core::AnimLayerId& target, const core::Frame::FrameData& data, size_t index);
+
+    //Implement later, brain fatigued
+    ::std::expected<void, core::ResolveFailure>
+    move_frame(core::AnimFile& file, core::AnimLayerId target, size_t frame, int64_t delta);
+
+    ::std::expected<core::FatFrame, FrameOpError>
+    get_frame(core::AnimFile& file, core::AnimLayerId target, size_t frame);
+
+    ::std::expected<::std::vector<core::FatFrame>, FrameOpError>
+    get_frames(core::AnimFile& file, core::AnimLayerId target, size_t frame, size_t count);
+}
