b3802c90fdc4c4fd736fc82aaeef93cc11baf8f2
skythedragon <skythedragon@sdf.org>
2026-03-16 19:16:33 -0700

    Added safety assertions to pun_cast

--- qtil/src/casts.hpp
+++ qtil/src/casts.hpp
@@ -9,7 +9,10 @@
 
 namespace qtil {
     template<typename T, typename U>
-    T pun_cast(U&& u) {
+    constexpr T pun_cast(U&& u) noexcept {
+        static_assert(sizeof(T) == sizeof(U), "Type punning requires input and output types to be the same size");
+        static_assert(std::is_trivially_copyable_v<T> && std::is_trivially_copyable_v<U>,
+                  "pun_cast requires trivially copyable types");
         return ::std::bit_cast<T>(::std::forward<U>(u));
     }
 }
