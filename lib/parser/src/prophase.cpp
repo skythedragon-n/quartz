b63e5d019cfe2591faf87227d5ba07751c284ccb
skythedragon <skythedragon@sdf.org>
2026-03-19 19:04:30 -0700

    Added parser to CMake config

---
+++ lib/parser/src/prophase.cpp
@@ -0,0 +1,11 @@
+//
+// copyleft 🄯 2026 by SkyTheDragon
+// licenced under the GPLv3-or-later, details located in LICENSE
+//
+// Created by skythedragon on 19 Mar 2026.
+//
+
+#include "prophase.hpp"
+
+namespace quartz::lib::parser {
+}
