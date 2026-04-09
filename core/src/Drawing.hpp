678605931e72f8596228eb4d884e4aadde926622
skythedragon <skythedragon@sdf.org>
2026-04-09 10:02:25 -0700

    Documented drawing

--- core/src/Drawing.hpp
+++ core/src/Drawing.hpp
@@ -25,15 +25,49 @@ namespace quartz::core {
 
         Drawing(IdKey, AnimFile*, DrawingId);
 
+        /**
+         * @brief Adds a new Stroke to the drawing
+         * @param color Color of new stroke
+         * @param thickness Thickness of new stroke
+         * @param corner_type Type of corner of new stroke
+         * @param miter_limit Stroke's miter limit
+         */
         void add_stroke(Color color, num_t thickness, CornerType corner_type = CornerType::Round, num_t miter_limit = 10.0);
+        /**
+         * @brief Gets stroke at index
+         * @param index Index of stroke to be gotten
+         * @return Stroke at index
+         */
         Stroke& get_stroke(size_t index);
 
+        /**
+         * @brief Adds a new point to the stroke
+         * @param point Point to be added
+         */
         void add_point(const Point& point);
 
+        /**
+         * @brief Gets an iterable thing over all of the Points in the Drawing
+         * @return Iterable thing
+         */
         ::qtil::VecIterable<Point> points() { return ::qtil::VecIterable{points_}; }
+        /**
+         * @brief Gets an iterable thing over all of the Strokes in the Drawing
+         * @return Iterable thing
+         */
         ::qtil::VecIterable<Stroke> strokes() { return ::qtil::VecIterable{strokes_}; }
 
+        /**
+         * @brief Indexes point
+         * @param index Index of point
+         * @return Point at index
+         */
         Point& operator[](size_t index);
+        /**
+         * @brief Indexes point, but const
+         * @param index Index of point
+         * @return Point at index
+         */
         const Point& operator[](size_t index) const;
     };
 }
