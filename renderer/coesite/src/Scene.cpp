43c0897408b45d54b681df33930fe3c36c9a3da1
skythedragon <skythedragon@sdf.org>
2026-06-29 16:36:34 -0700

    Added CMake for the new renderer.

---
+++ renderer/coesite/src/Scene.cpp
@@ -0,0 +1,11 @@
+//
+// copyleft 🄯 2026 by SkyTheDragon
+// licenced under the GPLv3-or-later, details located in LICENSE
+//
+// Created by skythedragon on 27 Jun 2026.
+//
+
+#include "Scene.hpp"
+
+namespace quartz::renderer::coesite {
+}
---
+++ renderer/coesite/src/Scene.hpp
@@ -0,0 +1,13 @@
+//
+// copyleft 🄯 2026 by SkyTheDragon
+// licenced under the GPLv3-or-later, details located in LICENSE
+//
+// Created by skythedragon on 27 Jun 2026.
+//
+
+#pragma once
+
+namespace quartz::renderer::coesite {
+    class Scene {
+    };
+}
